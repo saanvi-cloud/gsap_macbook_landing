02c5424747d172efdefdbde703bf73047ee80272
saanvi-cloud <saanvidd2003@gmail.com>
2025-11-16 04:57:21 -0800

    Implement Performance Section

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import ProductViewer from "./components/ProductViewer";
 import gsap from "gsap";
 import { ScrollTrigger, SplitText } from "gsap/all";
 import Showcase from "./components/Showcase";
+import Performance from "./components/Performance";
 import Features from "./components/Features";
 import Highlights from "./components/Highlights";
 import Footer from "./components/Footer";
